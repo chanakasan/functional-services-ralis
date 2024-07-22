e32a24a5649ecbbcd33dd503fb54ab7d963fd9e8
Chanaka Abeysinghe <chanakasan@gmail.com>
2024-07-22 13:45:09 +0530

    add users table

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+[
+  { full_name: "User One", email: "user-one@example.com" },
+  { full_name: "User Two", email: "user-two@example.com" },
+  { full_name: "User Three", email: "user-three@example.com" },
+  { full_name: "User Four", email: "user-four@example.com" },
+  { full_name: "User Five", email: "user-five@example.com" },
+  { full_name: "User Six", email: "user-six@example.com" },
+].map do |params|
+  User.create!(params)
+end
