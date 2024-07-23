540d3b7abe18282a5bb20d5ec3228b7f65eab3e3
Chanaka Abeysinghe <chanakasan@gmail.com>
2024-07-23 09:40:33 +0530

    display results

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
   def home
+    @result_one = GenerateUserDataService.call(Date.current - 7.days, Date.current) 
+    @result_two = AppServices.generate_users_csv(Date.current - 7.days, Date.current)
   end
 end
